cb2b66c93c936e2ae044753488c0f59c96c5550e
Khaoula Adjane <kadjane@e1r13p9.1337.ma>
2022-09-09 19:31:54 +0100

    v2

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: kadjane <kadjane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 17:40:53 by kadjane           #+#    #+#             */
-/*   Updated: 2022/09/08 18:58:34 by kadjane          ###   ########.fr       */
+/*   Updated: 2022/09/09 19:03:04 by kadjane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,15 @@ void	add_node(t_list **list, t_list *node)
 	*list = node;
 }
 
-void	remove_node(t_list *stack, t_list *node)
+t_list	*remove_node(t_list **stack, t_list *node)
 {
 	t_list **temp;
 
-	temp = &stack;
+	temp = stack;
 	while(*temp != node)
 		temp = &(*temp)->next;
-	temp = node->next;
+	*temp = node->next;
+	return *stack;
 }
 
 void affiche_stack(t_list *stack)
@@ -58,18 +59,26 @@ int	main(int ac, char **av)
 	t_list	*head_a;
 	t_list	*stack_b;
 	t_list	*head_b;
+	t_list **temp;
 	int	n;
 
-	head_a = NULL;
-	head_b = NULL;
+	stack_a = NULL;
+	stack_b = NULL;
 	if (ac-- > 1 && head_a == NULL)
 	{
 		n = ft_atoi(av[ac]);
-		head_a = node(n);
-		stack_a = head_a;
+		stack_a = node(n);
+		head_a = stack_a;
 		while(ac >= 1)
-			add_node(&stack_a, node(ft_atoi(av[ac--])));
-		affiche_stack(stack_a);
-		affiche_stack(stack_b);
+			add_node(&head_a, node(ft_atoi(av[ac--])));
+		affiche_stack(head_a);
+		temp = &head_a;
+		while(*temp != NULL)
+		{
+			if((*temp)->value == 1)
+				remove_node(&head_a,*temp);
+			temp =&(*temp)->next;
+		}
+		affiche_stack(head_a);
 	}
 }
